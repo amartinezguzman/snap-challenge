e7b5e0a241cb0587fd6aa7dd4f4c2c2def0bb472
Adriana Martinez <amartinezguzma@cnm.edu>
2021-07-30 08:58:13 -0600

    code along for using event attributes to change button color

---
+++ week-03/script.js
@@ -0,0 +1,8 @@
+function handleClick() {
+    const button = document.getElementById("target")
+    if (button.style.backgroundColor === "blue") {
+        button.style.backgroundColor = ""
+    } else {
+        button.style.backgroundColor = "blue"
+    }
+}
