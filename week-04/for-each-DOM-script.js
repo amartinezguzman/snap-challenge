fbe04cad21d25d3ed1fcf6928abe578c584a6300
Adriana Martinez <amartinezguzma@cnm.edu>
2021-08-02 09:03:37 -0600

    code along-using for each loop to change the DOM.

---
+++ week-04/for-each-DOM-script.js
@@ -0,0 +1,17 @@
+//Using the foreach loop display this array of text to the DOM.
+//
+//['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah']
+//
+// Hint: Each array element must appear in it's own html tag in the browser. Extra Credit: use map or reduce.
+
+
+const names = ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah']
+
+let target = document.getElementById('target')
+console.log(target)
+
+names.forEach(name=>target.innerHTML = target.innerHTML + `<p>${name}</p>`)
+
+let p= document.createElement('p')
+p.innerText = 'thi is a p tag'
+console.log(p)
